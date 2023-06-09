d233bd4514bbb9223e0b3003b6f22fd0cef59210
Luis Gontarski <luisedgon@outlook.com>
2023-06-09 18:16:51 -0300

    Código Final

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
     $dbHost = 'Localhost';
     $dbUsername = 'root';
-    $dbPassword = 'lu@@2013';
+    $dbPassword = '********';
     $dbName = 'allluga';
 
     /* mysqli O MySQLi é tanto processual quanto orientada por objeto – este último atributo foi herdado da versão mais antiga do MySQL. */
